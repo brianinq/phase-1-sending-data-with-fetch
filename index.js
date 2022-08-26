47788a93e3dd6ee67f529d1453d3cb9995396eab
brian <kipkoechbrian0@gmail.com>
2022-08-26 13:25:19 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+    name: name,
+    email: email,
+    };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData)
+  })
+  .then((res)=>res.json())
+  .then((json)=>{
+    document.body.innerHTML = json
+    return
+    })
+  .catch((err)=>{
+    const div = document.createElement("div")
+    const body = document.querySelector("body")
+    div.textContent = err.message
+    body.appendChild(div)
+  })
+}
